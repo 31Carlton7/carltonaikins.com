dfa88548990187997eb55f65de6008a5f07dac8d
Carlton Aikins <caikins317@gmail.com>
2023-12-29 14:07:15 +0000

    Update main components

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,7 +2,27 @@
 	export let pageName = '';
 </script>
 
-<footer class="bg-transparent p-10 w-full absolute bottom-0 z-[1000]">
+<footer class="bg-transparent w-full">
+	<div class="max-w-screen-xl flex flex-wrap justify-between items-center">
+		<div class="social-buttons">
+			<a href="https://www.github.com/31carlton7" target="_blank"
+				><i class="fa-brands fa-github text-2xl m-4 text-color3" /></a
+			>
+			<a href="https://www.linkedin.com/in/carltonaikins" target="_blank"
+				><i class="fa-brands fa-linkedin text-2xl m-4 text-color3" /></a
+			>
+			<a href="https://www.instagram.com/31carlton7" target="_blank"
+				><i class="fa-brands fa-instagram text-2xl m-4 text-color3" /></a
+			>
+			<a href="https://www.twitter.com/31carlton7" target="_blank"
+				><i class="fa-brands fa-twitter text-2xl m-4 text-color3" /></a
+			>
+		</div>
+		<div class="text-5xl italic font-thin text-color3">{pageName}</div>
+	</div>
+</footer>
+
+<!-- <footer class="bg-transparent p-10 w-full absolute bottom-0 z-[1000]">
 	<div class="max-w-screen-xl flex flex-wrap justify-between items-center">
 		<div class="social-buttons">
 			<a href="https://www.github.com/31carlton7" target="_blank"
@@ -22,4 +42,4 @@
 			{pageName}
 		</div>
 	</div>
-</footer>
+</footer> -->
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -3,11 +3,15 @@
 	import Menu from 'svelte-material-icons/Menu.svelte';
 	import Close from 'svelte-material-icons/Close.svelte';
 
+	$: outerWidth = 0;
+
 	export let menuOpen: boolean = false;
 </script>
 
+<svelte:window bind:outerWidth />
+
 <nav class="w-full top-0 left-0 absolute z-[10]">
-	<div class="md:flex items-center justify-between py-4 md:px-10 px-7">
+	<div class="md:flex md:flex-wrap items-center justify-between py-4 md:px-10 px-7">
 		<div class="flex items-center justify-center">
 			<Logo />
 		</div>
@@ -38,13 +42,27 @@
 				>
 			</li>
 			<li>
-				<a href="./resume.pdf" target="_blank" class="block py-2 pl-3 pr-4 md:border-0 md:p-0">
-					<div
-						class="border rounded-full px-4 py-2 text-white hover:text-color4 border-color3 hover:bg-color3 transition ease-in-out"
+				{#if outerWidth <= 768}
+					<a
+						href="https://www.carltonaikins.com/resume.pdf"
+						target="_blank"
+						class="block py-2 pl-3 pr-4 md:border-0 md:p-0"
 					>
 						Resume
-					</div>
-				</a>
+					</a>
+				{:else}
+					<a
+						href="https://www.carltonaikins.com/resume.pdf"
+						target="_blank"
+						class="block py-2 pl-3 pr-4 md:border-0 md:p-0"
+					>
+						<div
+							class="border rounded-full px-4 py-2 text-white hover:text-color4 border-color3 hover:bg-color3 transition ease-in-out"
+						>
+							Resume
+						</div>
+					</a>
+				{/if}
 			</li>
 		</ul>
 		<div class="absolute right-8 top-6 cursor-pointer md:hidden">
