77555e8eaff957cb83a86c86ad3c04f386f46288
Carlton Aikins <caikins317@gmail.com>
2023-12-29 14:05:55 +0000

    Finalize projects section

--- src/routes/projects/+layout.svelte
+++ src/routes/projects/+layout.svelte
@@ -6,12 +6,12 @@
 	import '@fontsource-variable/dm-sans';
 </script>
 
-<section class="bg-[#25262D] h-full overflow-hidden">
+<section class="bg-[#25262D]">
 	<GrainEffect />
 	<StripePattern />
 	<Navbar />
 	<main
-		class="relative flex flex-col h-screen items-center justify-center place-items-center py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-32 lg:px-12"
+		class="relative flex flex-col flex-wrap items-center justify-center place-items-center py-8 px-4 mx-auto max-w-screen-xl text-center md:py-32 md:px-12"
 	>
 		<slot />
 	</main>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,42 @@
-<script>
+<script lang="ts">
 	import Footer from '$lib/Footer.svelte';
 </script>
 
-Projects
+<div class="max-w-[1240px] mx-auto max-xl:mx-3 md:mt-0 md:mb-0 mt-12 mb-0 sm:mb-0">
+	<div class="flex w-full items-center justify-center">
+		<a
+			href="https://www.sections.vip"
+			target="_blank"
+			class="flex items-center justify-center bg-black md:rounded-3xl rounded-xl md:w-[65%] max-md:max-w-none max-md:p-8"
+		>
+			<img src="/images/sections-banner.png" alt="sections banner" draggable="false" />
+		</a>
+	</div>
+	<div
+		class="grid md:grid-cols-[1fr_1fr] max-md:grid-cols-[1fr] grid-rows-[auto] md:my-8 md:space-x-10 md:space-y-0 space-y-8"
+	>
+		<div class="flex flex-col w-full md:items-end items-center space-y-8 md:mt-0 mt-8">
+			<div
+				class="flex items-center justify-center bg-transparent md:rounded-3xl rounded-xl md:w-[65%] max-md:max-w-none"
+			>
+				<img src="/images/charm-technologies-banner.png" alt="charm technologies banner" />
+			</div>
+			<a
+				href="https://apps.apple.com/lc/app/elisha/id1608063814"
+				target="_blank"
+				class="flex items-center justify-center bg-transparent md:rounded-3xl rounded-xl md:w-[65%] max-md:max-w-none"
+			>
+				<img src="/images/elisha-banner.png" alt="elisha banner" />
+			</a>
+		</div>
+		<a class="flex w-full" href="https://www.clubology.co" target="_blank">
+			<div
+				class="flex justify-start bg-transparent md:rounded-3xl rounded-xl md:w-[58%] max-md:max-w-none"
+			>
+				<img src="/images/clubology-banner.png" alt="sections banner" class="" />
+			</div>
+		</a>
+	</div>
+</div>
+
 <Footer pageName="Projects" />
