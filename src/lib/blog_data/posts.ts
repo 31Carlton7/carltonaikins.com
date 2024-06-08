9b4023998b21c88cccba0bcab293570004a76496
Carlton Aikins <carlton@charmtechnologies.co>
2024-06-08 15:11:12 -0400

    [PATCH] Update the reading time to be more accurate

--- src/lib/blog_data/posts.ts
+++ src/lib/blog_data/posts.ts
@@ -44,7 +44,7 @@ export const posts = Object.entries(import.meta.glob('/blog_posts/**/*.md', { ea
 			},
 
 			// get estimated reading time for the post
-			readingTime: readingTime(html.structuredText, { wordsPerMinute: 265 }).text
+			readingTime: readingTime(html.structuredText, { wordsPerMinute: 465 }).text
 		};
 	})
 	// sort by date
